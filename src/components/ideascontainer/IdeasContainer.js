4e1b7c1eb1a67f76ae37953e944cec02bd49b37c
Eduardo <jeduardorjx@gmail.com>
2020-03-17 02:16:44 -0600

    Method to render only the favorite cards, if there are no favorite cards it will render a message

--- src/components/ideascontainer/IdeasContainer.js
+++ src/components/ideascontainer/IdeasContainer.js
@@ -4,6 +4,19 @@ import Card from '../card/Card';
 
 const IdeasContainer = ({ ideas, deleteIdea, toggleFavorite, viewFavorites }) => {
 
+  const renderFavoriteCards = ideas => {
+    const favIdeas = ideas.filter(idea=> idea.fav === true);
+    if (favIdeas.length) {
+      return favIdeas.map(idea =>
+        <Card key={idea.id}
+          idea={idea}
+          deleteIdea={deleteIdea}
+          toggleFavorite={toggleFavorite} />
+      );
+    } else {
+      return <h1>Favorite Some Ideas!</h1>;
+    }
+  }
 
   return (
     <section className='ideascontainer-section'>
