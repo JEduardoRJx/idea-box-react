2e2f02aa3da999342a459e6c0a31503a4eac0139
Eduardo <jeduardorjx@gmail.com>
2020-03-17 02:16:03 -0600

    Desctructure props and set JSX for component

--- src/components/ideascontainer/IdeasContainer.js
+++ src/components/ideascontainer/IdeasContainer.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import './IdeasContainer.scss';
 import Card from '../card/Card';
+
+const IdeasContainer = ({ ideas, deleteIdea, toggleFavorite, viewFavorites }) => {
+
+
+  return (
+    <section className='ideascontainer-section'>
+
+    </section>
+  )
+}
+
+export default IdeasContainer;
