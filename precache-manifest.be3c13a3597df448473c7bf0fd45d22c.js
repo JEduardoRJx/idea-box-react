24f4a9fe8716c5d305dbac61398fb88e2c657161
Eduardo <jeduardorjx@gmail.com>
2020-03-17 03:13:32 -0600

    Updates

---
+++ precache-manifest.be3c13a3597df448473c7bf0fd45d22c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e6c0d3419355faa495c017502fcaf9",
+    "url": "/idea-box-react/index.html"
+  },
+  {
+    "revision": "4de30361bc5adce855f6",
+    "url": "/idea-box-react/static/css/main.58595cca.chunk.css"
+  },
+  {
+    "revision": "0c2ffddef61a4cb26bd1",
+    "url": "/idea-box-react/static/js/2.55050c17.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/idea-box-react/static/js/2.55050c17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de30361bc5adce855f6",
+    "url": "/idea-box-react/static/js/main.99283e34.chunk.js"
+  },
+  {
+    "revision": "9dd96706e00ddc835f5c",
+    "url": "/idea-box-react/static/js/runtime-main.3c2910a4.js"
+  }
+]);
