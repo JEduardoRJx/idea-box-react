ff6f14b92545d37756e4de0b262a58fbf2aea82b
Eduardo <jeduardorjx@gmail.com>
2020-03-17 02:07:27 -0600

    Set form jsx and newIdea object in state

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,2 +1,20 @@
 import React, { useState } from 'react';
 import './Form.scss';
+
+const Form = ({ submitIdea, numOfIdeas }) => {
+  const [newIdea, createNewIdea] = useState({
+    id: null,
+    title: '',
+    description: '',
+    fav: false
+  });
+
+
+  return (
+    <form>
+
+    </form>
+  );
+}
+
+export default Form;
