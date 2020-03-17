241dda8a286380b777e0b4fa674092db6fc4f88c
Eduardo <jeduardorjx@gmail.com>
2020-03-17 01:48:47 -0600

    Move app files into the app director and import need components

---
+++ src/app/App.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import './App.scss';
+import Form from '../components/form/Form';
+import IdeasContainer from '../components/ideascontainer/IdeasContainer';
